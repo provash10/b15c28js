85afc5fcc4fa06220efefe88c071304ae89f0ca9
provash10 <provash20cb@gmail.com>
2024-10-16 18:52:05 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+let marks;
+marks = 101;
+// marks = -02;
+
+if(marks >100 || marks <0){
+    console.log('Invalid Marks')
+}
+
+// if(marks>=80){
+//     console.log('The grade is: A+');
+// }
+
+else if(marks>=80){
+    console.log('The grade is: A+');
+}
+
+else if(marks >=70){
+    console.log('The grade is: A')
+}
+
+else if(marks >=60){
+    console.log('The grade is: A-')
+}
+
+else if(marks >=50){
+    console.log('The grade is: B')
+}
+
+else if(marks >=40){
+    console.log('The grade is: C')
+}
+else if(marks >=33){
+    console.log('The grade is: D')
+}
+
+else{
+    console.log('The grade is: F')
+}
+// ==================even or odd==================
+
+let number;
+
+number = 64564563;
+if (number % 2 == 0){
+    console.log("This is even number");
+}
+
+else{
+    console.log("This is an odd number")
+}
+
+// ===============================================
+
+let years;
+years = 2033;
+
+if(years % 4 == 0){
+    console.log('This is a Leap Year');
+}
+
+else{
+    console.log('This is a Leap Year');
+}
